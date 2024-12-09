63169d9531984ca1b3ddd9bd779fd6ae98918e5b
nsannier <Naofumi76@users.noreply.github.com>
2024-12-09 09:47:12 +0100

    Added sweet-curry

---
+++ sweet-curry.js
@@ -0,0 +1,34 @@
+// Sweet Curry
+console.log('---------- Sweet Curry ----------')
+
+
+function mult2(x) {
+    return function (y) {
+        return x * y
+    }
+}
+
+
+function add3(x) {
+    return function (y) {
+        return function (z) {
+            return x + y + z
+        }
+    }
+}
+
+
+function sub4(x) {
+    return function (y) {
+        return function (z) {
+            return function (w) {
+                return x - y - z - w
+            }
+        }
+    }
+}
+
+
+console.log(mult2(4)(2))
+console.log(add3(5)(2)(3))
+console.log(sub4(10)(2)(3)(4))
