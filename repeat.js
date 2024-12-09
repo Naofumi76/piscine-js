d5ff58a3fecc14f2ee45a0ad42da78990785d653
nsannier <Naofumi76@users.noreply.github.com>
2024-12-09 10:13:48 +0100

    Added repeat

---
+++ repeat.js
@@ -0,0 +1,15 @@
+// Repeat
+console.log('---------- Repeat ----------')
+
+function repeat(str, nb) {
+    if (typeof str!=='string' || typeof nb !=='number' || nb < 0) {
+        throw new Error('Input must be a string and a positive number')
+    }
+    let result = ""
+    for (let i = 0; i < nb; i++) {
+        result += str
+    }
+    return result
+}
+
+console.log(repeat('Hello! ', 3))
