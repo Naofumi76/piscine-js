fe8e86fa6b4ef307623b56bfb1fc85f55b5388cb
nsannier <Naofumi76@users.noreply.github.com>
2025-01-06 09:40:05 +0100

    update

--- mutability.js
+++ mutability.js
@@ -1,9 +1,3 @@
-const person = {
-    name: 'Name',
-    age: 0,
-    country: 'COUNTRY'
-}
-
 const clone1 = Object.assign({}, person)
 const clone2 = Object.assign({}, person)
 const samePerson = person
