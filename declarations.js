63dc83ad194ea30a3218d9c960d55847073464d7
nsannier <Naofumi76@users.noreply.github.com>
2025-01-06 09:32:07 +0100

    update

--- declarations.js
+++ declarations.js
@@ -1,7 +1,10 @@
 const escapeStr = "` , \\ , / , \" and '"
-const arr = Object.freeze([4, '2'])
-const obj = Object.freeze({ str: "Chaine", num: 2, bool: true, undef:undefined})
-const nested = Object.freeze({ arr:[4, undefined, '2'], obj: {str:"Chaine", num: 2, bool: true}})
+const arr = [4, '2']
+const obj = { str: "Chaine", num: 2, bool: true, undef:undefined}
+const nested = { arr:[4, undefined, '2'], obj: {str:"Chaine", num: 2, bool: true}}
+Object.freeze(nested)
+Object.freeze(arr)
+Object.freeze(obj)
 
 console.log(obj)
 obj.str.update="TEST"
