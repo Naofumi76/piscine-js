fe7a9af503fac0e92b536e916b0671155e873334
nsannier <Naofumi76@users.noreply.github.com>
2025-01-06 11:05:49 +0100

    physics

--- physics.js
+++ physics.js
@@ -4,7 +4,7 @@ function getAcceleration(obj) {
     }
     if (obj.f !== undefined && obj.m !== undefined) {
         return obj.f / obj.m
-    } else if (obj.Δv !== undefined && obj.Δv !== undefined) {
+    } else if (obj.Δv !== undefined && obj.Δt !== undefined) {
         return obj.Δv / obj.Δt
     } else if (obj.t !== undefined && obj.d !== undefined) { 
         return (obj.d*2) / (obj.t*obj.t)
