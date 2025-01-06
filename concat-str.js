9c4fd3359f635b8ccbc73bccb23acc271db4b1dd
nsannier <Naofumi76@users.noreply.github.com>
2025-01-06 09:56:27 +0100

    update

--- concat-str.js
+++ concat-str.js
@@ -1,7 +1,3 @@
 function concatStr(str1, str2) {
-    if (typeof str1 === 'string' && typeof str2 === 'string') {
-        return str1 + str2
-    } else {
-        throw new Error('Input must be two strings')
-    }
+    return String(str1) + String(str2)
 }
