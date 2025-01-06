98b93d613c02803c4004421a8fd6cd933c36b44c
nsannier <Naofumi76@users.noreply.github.com>
2025-01-06 10:22:38 +0100

    dog-years

---
+++ dog-years.js
@@ -0,0 +1,23 @@
+const orbitalPeriods= {
+    earth: 1.0,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+}
+
+function dogyears(planet, yearInSeconds) {
+    const dogYearsPerHumanYear = 7
+    const secondsInEarthYear = 31557600
+
+    const earthYears = yearInSeconds / secondsInEarthYear
+
+    const planetYears = earthYears / orbitalPeriods[planet]
+
+    const dogYearsOnPlanet = planetYears * dogYearsPerHumanYear
+
+    return dogYearsOnPlanet.toFixed(2)
+}
