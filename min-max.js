c5343d39a9f981929a83e1f91b39c6a4382baecc
nsannier <Naofumi76@users.noreply.github.com>
2025-01-06 10:52:50 +0100

    min-max

---
+++ min-max.js
@@ -0,0 +1,14 @@
+function max(x1, x2) {
+    
+    if (x1 > x2) {
+        return x1
+    }
+    return x2
+}
+
+function min(x1, x2) {
+    if (x1 < x2) {
+        return x1
+    }
+    return x2
+}
