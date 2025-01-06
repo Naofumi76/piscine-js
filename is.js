36fc2ee3b9783390cb990173aee85dfd6a1ff2e7
nsannier <Naofumi76@users.noreply.github.com>
2025-01-06 10:54:12 +0100

    is

---
+++ is.js
@@ -0,0 +1,33 @@
+is.num = function num(x) {
+    return typeof x === 'number' && !Number.isNaN(x);
+}
+is.nan = function nan(x) {
+    return Number.isNaN(x)
+}
+is.str = function str(x) {
+    return typeof x ==='string'
+}
+is.bool = function bool(x) {
+    return typeof x === 'boolean'
+}
+is.undef = function undef(x) {
+    return x === undefined
+}
+is.def = function def(x) {
+    return x!== undefined
+}
+is.arr = function arr(x) {
+    return Array.isArray(x)
+}
+is.obj = function obj(x) {
+    return typeof x === 'object' && x !== null && !Array.isArray(x);
+}
+is.fun = function fun(x) {
+    return typeof x === 'function'
+}
+is.truthy = function truthy(x) {
+    return!!x
+}
+is.falsy = function falsy(x) {
+    return!x
+}
