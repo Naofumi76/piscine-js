47fb3ac04f9d139826a03d4ec63954ed8ef6482e
nsannier <Naofumi76@users.noreply.github.com>
2025-01-06 09:47:15 +0100

    more or less

---
+++ more-or-less.js
@@ -0,0 +1,17 @@
+function more(nb) {
+    nb++
+    return nb
+}
+
+function less(nb) {
+    nb--
+    return nb
+}
+
+function add(nb1, nb2) {
+    return nb1 + nb2
+}
+
+function sub(nb1, nb2) {
+    return nb1 - nb2
+}
