42062041cc5865ee8b97ad0020359208ab852a66
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 12:33:17 +0100

    reverser

--- reverser.js
+++ reverser.js
@@ -1,7 +1,15 @@
 function reverse(str) {
-    let result = ""
-    for (let i = str.length; i >= 0; i--) {
-        result += str.charAt(i)
+    if (typeof str === 'string') {
+        let result = ""
+        for (let i = str.length; i >= 0; i--) {
+            result += str.charAt(i)
+        }
+        return result
+    } else {
+        let result = []
+        for (let i = str.length - 1; i >= 0; i--) {
+            result.push(str[i])
+        }
+        return result
     }
-    return result
 }
