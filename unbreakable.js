17727055e38ffa80817f7933e0203f8a1313d0e1
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 12:58:14 +0100

    unbreakable

--- unbreakable.js
+++ unbreakable.js
@@ -1,38 +1,3 @@
 function split(str, sep) {
-    var parts = []
-    var temp = ""
-    var index = 0
-    while (index < str.length) {
-        var match = true
-        for (let i = 0; i < sep.length; i++) {
-            if (str[index + i] !== sep[i]) {
-                match = false
-                break        
-            }
-        }
-        if (match) {
-            parts.push(temp)
-            temp = ""
-            index += sep.length
-        } else {
-            temp += str[index]
-            index++
-        }
-    }
-    parts.push(temp)
-    return parts
+    return 0
 }
-
-function join(arr) {
-    var result = ""
-    for (let i = 0; i < arr.length; i++) {
-        result += arr[i]
-    }
-    return result
-}
-
-/*
-console.log(split('ggg - ddd - b', ' - '))
-console.log(split("ceci  est  un  test", "  "))
-console.log(join(['ceci est un test', 'test']))
-*/
