4720cca33b9f86113ffd00dbca081a5cc3754e2a
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 11:02:38 +0100

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -37,28 +37,6 @@ function ceil(nb) {
     return nbInt
 }
 
-function multiply(x1, x2) {
-    let oneNegative = (x1 < 0) !== (x2 < 0);
-    x1 = Math.abs(x1);
-    x2 = Math.abs(x2);
-    let result = 0;
-    for (let i = 0; i < x2; i++) {
-        result += x1;
-    }
-    return oneNegative ? -result : result;
-}
-
-function divide(x1, x2) {
-    let negativeResult = (x1 < 0) !== (x2 < 0);
-    x1 = Math.abs(x1);
-    x2 = Math.abs(x2);
-    let quotient = 0;
-    while (x1 >= x2) {
-        x1 -= x2;
-        quotient++;
-    }
-    return negativeResult ? -quotient : quotient;
-}
 
 function getCommaValue(nb) {
     if (nb < 0) {
@@ -73,18 +51,3 @@ function getCommaValue(nb) {
     return nb
 
 }
-
-function modulo(x1, y) {
-    let remainder = x1 - multiply(divide(x1, y), y);
-    if (x1 < 0 && remainder > 0) {
-        remainder -= Math.abs(y);
-    } else if (x1 > 0 && remainder < 0) {
-        remainder += Math.abs(y);
-    }
-    return remainder;
-}
-const nums = [3.7, -3.7, 3.1, -3.1]
-console.log(nums.map(round))
-console.log(nums.map(floor))
-console.log(nums.map(trunc))
-console.log(nums.map(ceil))
