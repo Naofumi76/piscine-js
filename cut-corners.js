d06cf3c2c7e07bb9af7e389c8323877395b0316d
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 09:46:33 +0100

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,7 +1,4 @@
 function round(nb) {
-    if (typeof nb !== 'number') {
-        throw new Error('Input must be a number')
-    }
     let nbInt = nb < 0 ? -parseInt(-nb) : parseInt(nb)
     let fracInt = nb - nbInt 
     if (fracInt >= 0.5) {
@@ -14,9 +11,6 @@ function round(nb) {
 }
 
 function floor(nb) {
-    if (typeof nb !== 'number') {
-        throw new Error('Input must be a number')
-    }
     let nbInt = nb < 0 ? -parseInt(-nb) : parseInt(nb)
     let fracInt = nb - nbInt 
     if (fracInt < 0) {
@@ -26,18 +20,12 @@ function floor(nb) {
 }
 
 function trunc(nb) {
-    if (typeof nb!== 'number') {
-        throw new Error('Input must be a number')
-    }
     let nbInt = nb < 0 ? -parseInt(-nb) : parseInt(nb)
     return nbInt
 }
 
 
 function ceil(nb) {
-    if (typeof nb!== 'number') {
-        throw new Error('Input must be a number')
-    }
     let nbInt = nb < 0 ? -parseInt(-nb) : parseInt(nb)
     let fracInt = nb - nbInt 
     if (fracInt > 0) {
