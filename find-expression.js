7d4144699f1592a55f8ad335cff36ba557d21073
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 14:47:25 +0100

    find-expression

--- find-expression.js
+++ find-expression.js
@@ -11,7 +11,7 @@ function findExpression(number) {
             chaineresult += ' ' + add4
         }
         else {
-            return ''
+            return number
         }
     }
     return chaineresult.trim()
