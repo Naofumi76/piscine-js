8bb5ccc10079c795c629793737fac3fe74e564b8
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 14:12:58 +0100

    find-expression

---
+++ find-expression.js
@@ -0,0 +1,24 @@
+function findExpression(number) {
+    let result = 1
+    let chaineresult = "1 "
+    while (result != number) {
+        if ((result * 2) <= number && (result+4) !== number) {
+            result *= 2;
+            chaineresult += mul2 + ' ';
+        }
+        else if ((result + 4) <= number) {
+            result += 4;
+            chaineresult += add4 + ' ';
+        }
+        else {
+            return undefined;
+        }
+    }
+    return chaineresult.trim()
+}
+/*
+const add4 = '+4'
+const mul2 = '*2'
+
+console.log(findExpression(8))
+*/
