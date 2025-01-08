7cbd91f5898d355d622efb18e279e252fe8e2374
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 09:10:36 +0100

    update

--- elementary.js
+++ elementary.js
@@ -41,7 +41,6 @@ function modulo(x1, y) {
         throw new Error('Cannot divide by zero');
     }
     let remainder = x1 - multiply(divide(x1, y), y);
-    // Ensure the remainder matches the sign of the dividend
     if (x1 < 0 && remainder > 0) {
         remainder -= Math.abs(y);
     } else if (x1 > 0 && remainder < 0) {
@@ -49,20 +48,3 @@ function modulo(x1, y) {
     }
     return remainder;
 }
-
-console.log(multiply(10, 5))
-console.log(multiply(-10, 5))
-console.log(divide(10, 5))
-console.log(divide(-10, 5))
-console.log("Problem here :")
-console.log('divide(10, -3) : ', divide(10, -3))
-console.log('10%-3 : ', 10%-3)
-console.log('divide(-10, -3) : ', divide(-10, -3))
-console.log('-10%-3 : ', -10%-3)
-// console.log(divide(10, 0))
-console.log(modulo(10, 3))
-console.log("Problem here :")
-console.log('modulo(5-3) : ', modulo(5, -3))
-console.log('5%-3 : ', 5%-3)
-console.log(modulo(-3, -5))
-console.log(-3%-5)
