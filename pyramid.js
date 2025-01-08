f57133adf09da7ad4f5c0157b9d9b6a623a64123
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 13:56:20 +0100

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,7 @@
 function pyramid(str, depth) {
     let result = ''
     for (let i = 1; i <= depth; i++) {
-        for (let j = 0; j < depth - i; j++) {
+        for (let j = 0; j < str.length * (depth - i); j++) {
             result +=' '
         }
         for (let k = 0; k < 2 * i - 1; k++) {
@@ -13,3 +13,6 @@ function pyramid(str, depth) {
     }
     return result
 }
+
+
+//console.log(pyramid('{}', 12))
