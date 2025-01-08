a3271c143805c4a1a8f918781fdf1a75027e979c
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 12:17:28 +0100

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,53 +1,50 @@
-function round(nb) {
-    let fracInt = getCommaValue(nb)
-    let nbInt = nb-fracInt
-    if (fracInt >= 0.5) {
-        return nbInt+1
-    } else if (fracInt <= -0.5) {
-        return nbInt-1
-    } else {
-        return nbInt
-    }
-}
-
-function floor(nb) {
-    let fracInt = getCommaValue(nb)
-    let nbInt = nb-fracInt
-    if (fracInt < 0) {
-        return nbInt-1
-    }
-    return nbInt
-}
-
-function trunc(nb) {
-    let fracInt = getCommaValue(nb)
-    let nbInt = nb-fracInt
-    return nbInt
-}
-
-
-function ceil(nb) {
-    let fracInt = getCommaValue(nb)
-    let nbInt = nb-fracInt
-    if (fracInt > 0) {
-        return nbInt+1
-    } else if (fracInt < 0) {
-        return nbInt
-    }
-    return nbInt
-}
-
-
-function getCommaValue(nb) {
-    if (nb < 0) {
-        while (nb <= -1) {
-            nb += 1
-        }  
-    } else {
-        while (nb >= 1) {
-            nb -= 1
+//round nearest integer
+function round (n){
+    let decimal = (n*10)-trunc(n)*10;
+    if (decimal >= 5){return ceil(n)};
+    if (decimal < 0){
+        if (decimal > -5){
+            return ceil(n)
         }
-    }
-    return nb
+    };
+    return floor(n);
+};
 
+//round up
+function ceil (n){
+    if ( n > 0 ){n += 1};
+    return getDigit(n);
 }
+
+//round down
+function floor (n){
+    if ( n < 0 ){n -= 1};
+    return getDigit(n);
+};
+
+function trunc (n){
+    return getDigit(n);
+};
+
+function getDigit (n){
+    
+    let absA = Math.abs(n);
+    let result = 0;
+
+    while (absA >= 1){
+        let b = 1; 
+        while (b < (absA/10)){
+            b *= 10;
+        }
+        absA -= b;
+        result += b;
+    };
+    if ( n < 0 ){ result = -result;};
+    return result;
+}
+
+/*const nums = [3.7, -3.7, 3.1, -3.1]
+console.log(nums.map(round))
+console.log(nums.map(floor))
+console.log(nums.map(trunc))
+console.log(nums.map(ceil))*/
