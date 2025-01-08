3650f7af13a7543e03316efbfe6af8ca8fe6387b
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 13:27:17 +0100

    nasa

--- nasa.js
+++ nasa.js
@@ -8,16 +8,13 @@ function nasa(N) {
         } else if (i%5 === 0) {
             str += "SA "
         } else {
-            if (Number(i) != N) {
-                str += String(i)+ " "
-            } else {
-                str+= String(i)
-            }
+           str += String(i)+ " "
         }
     }
     str = str.substring(0, str.length-1)
+    //console.log(str[str.length-1])
     return str
 }
 
 
-console.log(nasa(15))
+//console.log(nasa(15))
