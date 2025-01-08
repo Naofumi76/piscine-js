fe112dd36276e8074c9087bad01da6466ed48538
nsannier <Naofumi76@users.noreply.github.com>
2025-01-08 09:52:39 +0100

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,5 +1,5 @@
 function round(nb) {
-    let nbInt = nb < 0 ? -parseInt(-nb) : parseInt(nb)
+    let nbInt = nb < 0 ? (nb - 1) | 0 : nb | 0;
     let fracInt = nb - nbInt 
     if (fracInt >= 0.5) {
         return nbInt+1
@@ -11,7 +11,7 @@ function round(nb) {
 }
 
 function floor(nb) {
-    let nbInt = nb < 0 ? -parseInt(-nb) : parseInt(nb)
+    let nbInt = nb < 0 ? (nb - 1) | 0 : nb | 0;
     let fracInt = nb - nbInt 
     if (fracInt < 0) {
         return nbInt-1
@@ -20,13 +20,13 @@ function floor(nb) {
 }
 
 function trunc(nb) {
-    let nbInt = nb < 0 ? -parseInt(-nb) : parseInt(nb)
+    let nbInt = nb < 0 ? (nb - 1) | 0 : nb | 0;
     return nbInt
 }
 
 
 function ceil(nb) {
-    let nbInt = nb < 0 ? -parseInt(-nb) : parseInt(nb)
+    let nbInt = nb < 0 ? (nb - 1) | 0 : nb | 0;
     let fracInt = nb - nbInt 
     if (fracInt > 0) {
         return nbInt+1
