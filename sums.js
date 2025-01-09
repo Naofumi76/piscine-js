3b65b67d00baa7bc9f8dfb22feccb746d99e0483
nsannier <Naofumi76@users.noreply.github.com>
2025-01-09 10:18:26 +0100

    sums

---
+++ sums.js
@@ -0,0 +1,24 @@
+function sums(a) {
+
+    function solve(nb, partition = [], start = 1){
+        if (typeof nb !== 'number') {
+            throw new Error('Input must be a number')
+        }
+        if (nb ===0) {
+            return [partition]
+        }
+        let result = []
+        for (let i = start; i <= nb; i++) {
+            let newPart = [...partition, i]
+            if(i !== a)result.push(...solve(nb-i, newPart, i));
+        }
+        return result
+    }
+
+    return solve(a);
+}
+
+
+console.log(sums(4))
+console.log(sums(6))
+console.log(sums(8))
