dafcc0730fd1c4ee9b17eb6c53fe8650a9252750
nsannier <Naofumi76@users.noreply.github.com>
2025-01-09 09:53:05 +0100

    find-expression

--- find-expression.js
+++ find-expression.js
@@ -1,24 +1,35 @@
 function findExpression(number) {
     let result = 1
     let chaineresult = "1"
-    while (result != number) {
-        if ((result * 2) <= number && (result * 2) <= number - 4) {
-            result *= 2;
-            chaineresult += ' ' + mul2
-        }
-        else if ((result + 4) <= number) {
-            result += 4;
-            chaineresult += ' ' + add4
-        }
-        else {
-            return undefined
-        }
-    }
-    return chaineresult.trim()
+
+    return solver(number, result, chaineresult)
 }
+
+function solver(number, calc, chaine) {
+    if (calc === number) {
+        return chaine
+    } else if (calc > number) {
+        return undefined
+    }
+
+    if (solver(number, calc+4, chaine + " " + add4) !== undefined) {
+        return solver(number, calc+4, chaine + " " + add4)
+    }
+
+    if (solver(number, calc*2, chaine + " " + mul2) !== undefined) {
+        return solver(number, calc*2, chaine + " " + mul2)
+    }
+
+    return undefined
+}
+
+
 /*
 const add4 = '+4'
 const mul2 = '*2'
 
 console.log(findExpression(14))
+console.log(findExpression(2))
+console.log(findExpression(12))
+console.log(findExpression(3))
 */
