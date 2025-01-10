6a1dc29f099476f507d9accf0e362e31b6faf5b8
nsannier <Naofumi76@users.noreply.github.com>
2025-01-10 09:48:46 +0100

    ion-out

--- ion-out.js
+++ ion-out.js
@@ -2,7 +2,7 @@ function ionOut(str) {
     if (typeof str!=='string') {
         throw new Error('Input must be a string')
     }
-    var reg = /(?<=t)(\w*?)ion/
+    var reg = /(\w*?)tion/
     var tab = []
     var arrStr = str.split(' ')
     for (let i = 0; i < arrStr.length; i++) {
@@ -14,4 +14,5 @@ function ionOut(str) {
     return tab
 }
 
-console.log(ionOut('attention, direction'))
+console.log('transfusion')
+console.log(ionOut('test reaction mange'))
