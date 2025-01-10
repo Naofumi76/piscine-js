8749eebb6d2f0844327b9b62af1d9c9e111d5583
nsannier <Naofumi76@users.noreply.github.com>
2025-01-10 09:26:39 +0100

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -2,10 +2,11 @@ function sameAmount(str, reg1, reg2) {
     if (typeof str !== 'string' || !(reg2 instanceof RegExp) || !(reg1 instanceof RegExp)) {
         throw new Error('Input must contain a string and two RegExp');
     }
-    let test1 = str.match(reg1)
-    let count1 = 0
-    let test2 = str.match(reg2)
-    let count2 = 0
+    const newReg1 = new RegExp(reg1.source, 'g')
+    const newReg2 = new RegExp(reg2.source, 'g')
+    let test1 = str.match(newReg1)
+    let count1, count2
+    let test2 = str.match(newReg2)
     if (test1) {
         count1 = test1.length
     }
