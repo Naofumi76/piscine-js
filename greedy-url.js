45a06236483ec722f0d8e310156ec194114b24a1
nsannier <Naofumi76@users.noreply.github.com>
2025-01-10 09:51:38 +0100

    greedy-url

---
+++ greedy-url.js
@@ -0,0 +1,49 @@
+function getURL(str) {
+    //var reg = /(https?:\/\/[^\s]+)/g
+
+    // This regex allows us to get the url starting with http or https, with all their query parameters.
+    var reg = /https?:\/\/[^\s?]+(?:\?([^#\s]*))?/g
+    var tabURL = str.match(reg)
+    if (!tabURL) {
+        return []
+    }
+    return tabURL
+}
+
+function greedyQuery(str) {
+    tabURL = getURL(str)
+    var result = []
+    for (var i = 0; i < tabURL.length; i++) {
+        var query = tabURL[i].match(/\?([^#\s]*)/)
+        if (query && query[1]) {
+            var params = query[1].split('&')
+            if (params.length >= 3) {
+                result.push(tabURL[i])
+            }
+        } 
+    }
+    return result
+}
+
+function notSoGreedy(str) {
+    tabURL = getURL(str)
+    var result = []
+    for (var i = 0; i < tabURL.length; i++) {
+        var query = tabURL[i].match(/\?([^#\s]*)/)
+        if (query && query[1]) {
+            var params = query[1].split('&')
+            if (params.length >= 2 && params.length <= 3) {
+                result.push(tabURL[i])
+            }
+        } 
+    }
+    return result
+}
+
+const dataset = `
+qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you
+`;
+
+console.log(getURL(dataset));
+console.log(greedyQuery(dataset));
+console.log(notSoGreedy(dataset));
