f3dd8d46f1a873634f93bb3f681d904e6981f09c
nsannier <Naofumi76@users.noreply.github.com>
2025-01-10 11:56:44 +0100

    valid-ip

--- valid-ip.js
+++ valid-ip.js
@@ -1,13 +1,33 @@
 function findIP(str) {
     // Regex to match IP addresses (nnn.nnn.nnn.nnn) and ports (:xxxxx).
     var regex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d{1,5}))?/g
+    var ipRegex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/
     var portRegex = /:(\d{1,5})/g
     var matchesIP = str.match(regex)
     var matchesPort = []
     if (!matchesIP) {
         return []
     } 
+    var validIPS = []
     for (var match of matchesIP) {
+        var ip = match.match(ipRegex)
+        if (ip) {
+            var ipVals = (ip[0].split("."))
+            var isValid = true
+            for (var ipv of ipVals) {
+                var part = parseInt(ipv)
+                if (part < 0 || part > 255) {
+                    isValid = false
+                    break
+                }
+            }
+            if (isValid) {
+                validIPS.push(match)
+            }
+        }
+    }
+
+    for (var match of validIPS) {
         var port = match.match(portRegex)
         if (port) {
             port = parseInt(String(port[0]).replace(":", ""))
