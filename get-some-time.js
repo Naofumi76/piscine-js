26e6948872c3ec1e0f6049bf148a2002030083c8
nsannier <Naofumi76@users.noreply.github.com>
2025-01-13 10:58:46 +0100

    get-some-time

--- get-some-time.js
+++ get-some-time.js
@@ -1,36 +1,21 @@
-function firstDayWeek(nbWeek, strYear) {
-    if (typeof strYear !== 'string' || typeof nbWeek !== 'number') {
-        return;
-    }
-
-    if (nbWeek <= 0 || nbWeek > 53) {
-        return;
-    }
-
-    var year = parseInt(strYear, 10);
-    var janFirst = new Date(`${year}-01-01`);
-    var janFirstDay = janFirst.getDay();
-    var offsetToMonday = (janFirstDay === 0 ? 6 : janFirstDay - 1);
-  
-    var firstMonday = new Date(janFirst);
-    firstMonday.setDate(janFirst.getDate() - offsetToMonday);
-
-    var desiredWeekStart = new Date(firstMonday);
-    desiredWeekStart.setDate(firstMonday.getDate() + (nbWeek - 1) * 7);
-  
-    if (nbWeek === 1 && desiredWeekStart.getFullYear() < year) {
-        var day = String(janFirst.getDate()).padStart(2, '0');
-        var month = String(janFirst.getMonth() + 1).padStart(2, '0');
-        return `${day}-${month}-${strYear}`;
-    }
-    var day = String(desiredWeekStart.getDate()).padStart(2, '0');
-    var month = String(desiredWeekStart.getMonth() + 1).padStart(2, '0');
-    year = desiredWeekStart.getFullYear();
-
+function formatDate(date, year) {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     return `${day}-${month}-${year}`;
 }
 
-console.log(firstDayWeek(1, '1000'))
-console.log(firstDayWeek(1, '2022'))
-console.log(firstDayWeek(2, '2022'))
-console.log(firstDayWeek(3, '2022'))
+function firstDayWeek(weekNumber, year) {
+    const janFirst = new Date(`${year}-01-01`);
+    const janFirstDay = janFirst.getDay();
+    const offsetToMonday = (janFirstDay === 0 ? 6 : janFirstDay - 1);
+    const firstMonday = new Date(janFirst);
+    firstMonday.setDate(janFirst.getDate() - offsetToMonday);
+    const desiredWeekStart = new Date(firstMonday);
+    desiredWeekStart.setDate(firstMonday.getDate() + (weekNumber - 1) * 7);
+    if (desiredWeekStart.getFullYear() < parseInt(year)) {
+        return formatDate(janFirst, year);
+    }
+    return formatDate(desiredWeekStart,year);
+}
+
+console.log(firstDayWeek('2', '0001'))
