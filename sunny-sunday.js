be4b6c27a22835b576b0da48caa9c90651d50e24
nsannier <Naofumi76@users.noreply.github.com>
2025-01-13 11:42:45 +0100

    sunny-sunday

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,11 +1,12 @@
 function sunnySunday(date) {
-    let day = date.getDay();
-    const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    if (day === 0) {
-        return "Monday"
-    }
-    return weekdays[day - 1]
+    var weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var baseDate = new Date('0001-01-01')
+    var nbDays = Math.floor((new Date(date) - baseDate) / (1000 * 60 * 60 * 24))
+    var dayOfWeek = nbDays%6
+    return weekDays[dayOfWeek]
 }
+
+console.log(sunnySunday(new Date('0001-12-01')))
 console.log(sunnySunday(new Date('0001-01-01')))
 console.log(sunnySunday(new Date('0001-01-06')))
 console.log(sunnySunday(new Date('0001-01-07')))
