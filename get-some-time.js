bbe91c48d7cd815e50e599fc62ea720d0a91e469
nsannier <Naofumi76@users.noreply.github.com>
2025-01-13 10:47:48 +0100

    get-some-time

--- get-some-time.js
+++ get-some-time.js
@@ -1,29 +1,33 @@
 function firstDayWeek(nbWeek, strYear) {
     if (typeof strYear !== 'string' || typeof nbWeek !== 'number') {
-        return
+        return;
     }
+
     if (nbWeek <= 0 || nbWeek > 53) {
-        return
+        return;
     }
-    let date = new Date(strYear, 0, 1)
-    let day = date.getDay()
-    let dayUntilMonday = (day ===0) ? 1 : (7 - day + 1) % 7
 
-    date.setDate(date.getDate() + dayUntilMonday)
-    
+    var year = parseInt(strYear, 10);
+    var janFirst = new Date(`${year}-01-01`);
+    var janFirstDay = janFirst.getDay();
+    var offsetToMonday = (janFirstDay === 0 ? 6 : janFirstDay - 1);
+  
+    var firstMonday = new Date(janFirst);
+    firstMonday.setDate(janFirst.getDate() - offsetToMonday);
 
-    let firstDayWeek = new Date(date)
-    if (nbWeek === 1 && firstDayWeek.getFullYear() !== parseInt(strYear)) {
-        return `01-01-${strYear}`
+    var desiredWeekStart = new Date(firstMonday);
+    desiredWeekStart.setDate(firstMonday.getDate() + (nbWeek - 1) * 7);
+  
+    if (nbWeek === 1 && desiredWeekStart.getFullYear() < year) {
+        var day = String(janFirst.getDate()).padStart(2, '0');
+        var month = String(janFirst.getMonth() + 1).padStart(2, '0');
+        return `${day}-${month}-${strYear}`;
     }
-    firstDayWeek.setDate(firstDayWeek.getDate() + 7 * (nbWeek - 1))
+    var day = String(desiredWeekStart.getDate()).padStart(2, '0');
+    var month = String(desiredWeekStart.getMonth() + 1).padStart(2, '0');
+    year = desiredWeekStart.getFullYear();
 
-    // using padStart allow to add '0' before the day/month string if the day/month's digit is only one character
-    day = firstDayWeek.getDate().toString().padStart(2, '0')
-    let month = (firstDayWeek.getMonth()+1).toString().padStart(2, '0')
-    let year = firstDayWeek.getFullYear()
-
-    return `${day}-${month}-${year}`
+    return `${day}-${month}-${year}`;
 }
 
 console.log(firstDayWeek(1, '1000'))
