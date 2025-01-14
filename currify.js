0581b010272c266f9f2a1675f415f4e994d8549c
nsannier <Naofumi76@users.noreply.github.com>
2025-01-14 13:36:47 +0100

    currify

---
+++ currify.js
@@ -0,0 +1,16 @@
+function currify(func) {
+    return function (...args) {
+        if (args.length >= func.length) {
+            return func(...args)
+        }
+        return (...moreArgs) => curried(...args,...moreArgs)
+    }
+}
+
+const mult2 = (el1, el2) => el1 * el2
+console.log(mult2(2, 2)) // result expected 4
+
+const mult2Curried = currify(mult2)
+
+console.log(mult2Curried(2)(2)) // result expected 4
+// (same result, with a function that has technically only one argument)
