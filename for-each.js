3695ebda374e9b4e43223ac472d658fe77adc8a0
nsannier <Naofumi76@users.noreply.github.com>
2025-01-14 10:06:42 +0100

    for-each

--- for-each.js
+++ for-each.js
@@ -7,8 +7,22 @@ function forEach(callback, arr) {
     }
 }
 
+const testArr = [1, 2, 3]
+var result = []
+forEach(value => result.push(value), testArr)
+console.log(result)
 
-const arr = [1, 2, 3, 4, 5]
+const ctx = {
+    arr: [1, 2, 3, 4, 5, 0.31087952857109147, 7, 10, -10, 20, -95]
+}
+result = []
+const returned = forEach(value => result.push(value), ctx.arr)
+console.log(returned)
+console.log(result)
+
+
+
+/* const arr = [1, 2, 3, 4, 5]
 function show(number) {
     console.log(number)
 }
@@ -23,4 +37,4 @@ forEach(add1, arr)
 console.log(arr)
 forEach(show, arr)
 forEach(remove1, arr)
-console.log(arr)
+console.log(arr) */
