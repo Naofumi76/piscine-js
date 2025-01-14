7d877ebec760a1b61fd3329e8869061dd9187211
nsannier <Naofumi76@users.noreply.github.com>
2025-01-14 11:52:44 +0100

    reduce

--- reduce.js
+++ reduce.js
@@ -38,8 +38,8 @@ function reduceRight(arr, func, accumulator) {
     return foldRight(arr, func, accumulator)
 }
 
-const adder = (a, b) => a + b
+/* const adder = (a, b) => a + b
 console.log(fold([1, 2, 3], adder, 2)) // returns 8 (2 + 1 + 2 + 3)
 console.log(foldRight([1, 2, 3], adder, 2)) // returns 8 (2 + 3 + 2 + 1)
 console.log(reduce([1, 2, 3], adder)) // returns 6 (1 + 2 + 3)
-console.log(reduceRight([1, 2, 3], adder)) // returns 6 (3 + 2 + 1)
+console.log(reduceRight([1, 2, 3], adder)) // returns 6 (3 + 2 + 1) */
