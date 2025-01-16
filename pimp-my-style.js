772b88423f7c34be1113b0180b650875d9ec8c2f
nsannier <Naofumi76@users.noreply.github.com>
2025-01-16 12:07:40 +0100

    pimp-my-style

---
+++ pimp-my-style.js
@@ -0,0 +1,21 @@
+import { styles } from './pimp-my-style.data.js'
+var stylesIndex = 0
+var unpimping = false
+export function pimp() {
+    var button = document.getElementsByTagName('button')[0]
+    if (!unpimping) {
+        button.classList.add(styles[stylesIndex])
+        stylesIndex++
+        if (stylesIndex === styles.length) {
+            unpimping = true
+            button.classList.add('unpimp')
+        }
+    } else if (unpimping) {
+        button.classList.remove(styles[stylesIndex - 1])
+        stylesIndex--
+        if (stylesIndex === 0) {
+            button.textContent = 'pimp my style'
+            unpimping = false
+        }
+    }
+}
