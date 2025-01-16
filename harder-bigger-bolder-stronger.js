28e5d4d20e1f29081bf6a242a04ce6f059048232
nsannier <Naofumi76@users.noreply.github.com>
2025-01-16 11:06:03 +0100

    harder-bigger-bolder-stronger

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,22 @@
+var fontSize = 10
+export function generateLetters() {
+    var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var choosenLetters = []
+    for (let i = 0; i < 120; i++) {
+        choosenLetters.push(letters[Math.floor(Math.random() * letters.length)]);
+    }
+    for (var i = 0; i < choosenLetters.length; i++ ){
+        var letterDiv = document.createElement('div')
+        letterDiv.textContent = choosenLetters[i]
+        letterDiv.style.fontSize = String(fontSize) + 'px'
+        if (i < letters.length/3) {
+            letterDiv.style.fontWeight = '300'
+        } else if (i < (letters.length/3)*2) {
+            letterDiv.style.fontWeight = '400'
+        } else {
+            letterDiv.style.fontWeight = '600'
+        }
+        fontSize++
+        document.body.appendChild(letterDiv)
+    }
+}
