90dba94c520051de62f6bf4e6332f7fc648a6304
nsannier <Naofumi76@users.noreply.github.com>
2025-01-21 08:34:33 +0100

    manipulate-entries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -20,42 +20,53 @@ function mapEntries(obj, func) {
 function reduceEntries(obj, func, acc) {
     let result = acc
     for (let [key, value] of Object.entries(obj)) {
-        result = func(result, key, value)
+        result = func(result, [key, value])
     }
     return result
 }
 
 function totalCalories(obj) {
-    return reduceEntries(obj, (acc, key, value) => acc + value, 0)
+    return reduceEntries(obj, (acc, key) => {
+        var result = (nutritionDB[key[0]]["calories"] / 100) * key[1]
+        return acc + result
+    },0).toFixed(1)
 }
 
 function lowCarbs(obj) {
-    return filterEntries(obj, (key, value) => value <= 50)
+    return filterEntries(obj, (key, value) => {
+        return (nutritionDB[key].carbs / 100 * value) < 50
+    })
 }
 
 function cartTotal(obj) {
-    return reduceEntries(obj, (acc, key, value) => {
-        acc[key] = (acc[key] || 0) + value
-        return acc
-    }, {})
+    var result = {}
+    for (var ingredient in obj) {
+        result[ingredient] = {}
+        for (var nutriKey in nutritionDB[ingredient]) {
+            // multiply by 1000 the calcs, and divide then by 1000 after calculating allow for 3 digits after comma of accuracy
+            result[ingredient][nutriKey] = Math.round((obj[ingredient] / 100) * nutritionDB[ingredient][nutriKey] * 1000) / 1000
+        }
+    }
+    return result
 }
+const nutritionDB = {
+    tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+   vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+    oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+    onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+    garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+    paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+    sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+    orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
+  }
+  
 
-// Test Data
-const groceriesCart1 = {
-    onion: 230,
-    garlic: 220,
-    chocolate: 500,
-    sugar: 700,
-};
+const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
 
-console.log(filterEntries(groceriesCart1, (key, value) => value < 300));
-// Expected output: { onion: 230, garlic: 220 }
+console.log('Total calories:')
+console.log(totalCalories(groceriesCart))
+console.log('Items with low carbs:')
+console.log(lowCarbs(groceriesCart))
+console.log('Total cart nutritional facts:')
+console.log(cartTotal(groceriesCart))
 
-console.log(totalCalories(groceriesCart1));
-// Expected output: 1650 (230 + 220 + 500 + 700)
-
-console.log(lowCarbs(groceriesCart1));
-// Expected output: { onion: 230, garlic: 220 }
-
-console.log(cartTotal(groceriesCart1));
-// Expected output: { onion: 230, garlic: 220, chocolate: 500, sugar: 700 }
