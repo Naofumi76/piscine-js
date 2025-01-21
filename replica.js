57148e65319595da8ec8c5f4a09465f44e259174
nsannier <Naofumi76@users.noreply.github.com>
2025-01-21 14:03:47 +0100

    replica

--- replica.js
+++ replica.js
@@ -1,3 +1,21 @@
-function replica() {
-
+function replica(targetObj, ...otherObj) {
+    for (var obj of otherObj){
+        if (typeof obj === 'object') {
+            for (var key of obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (obj[key] instanceof Date) {
+                        targetObj[key] = new Date(obj[key])
+                    } else if (obj[key] instanceof RegExp) {
+                        targetObj[key] = new RegExp(obj[key])
+                    } else if (typeof obj[key] === 'object') {
+                        targetObj[key] = Array.isArray(obj[key])? [] : {}
+                        replica(targetObj[key], obj[key])
+                    } else {
+                        targetObj[key] = obj[key]
+                    }
+                }
+            }
+        }
+    }
+    return targetObj
 }
