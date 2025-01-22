8f58808e1448dbe8594abb7e2da31ed12fbb3c65
nsannier <Naofumi76@users.noreply.github.com>
2025-01-22 08:51:35 +0100

    debounce

--- debounce.js
+++ debounce.js
@@ -8,11 +8,39 @@ function debounce(func, delay) {
     }
 }
 
-/* function printing() {
+function opDebounce(func, delay, leading) {
+    var debounceTimer
+    var hasLeading = false
+    return function () {
+        const context = this
+        const args = arguments
+        clearTimeout(debounceTimer)
+        if (leading && !hasLeading) {
+            func.apply(context, args)
+            hasLeading = true
+            return
+        }
+        debounceTimer = setTimeout(() => {
+            hasLeading = false
+            func.apply(context, args)
+        }, delay)
+    }
+}
+
+function printing() {
     console.log("Printing!")
 }
 
-var debouncedPrinting = debounce(printing, 1000)
-debouncedPrinting()
-debouncedPrinting = debounce(printing, 5000)
-debouncedPrinting() */
+function debouncedPrinting() {
+    console.log("Debounced printing!")
+}
+
+var debounceTest = debounce(printing, 1000)
+debounceTest()
+debounceTest = debounce(printing, 5000)
+debounceTest()
+
+var opDebounceTest = opDebounce(debouncedPrinting, 1000, true)
+opDebounceTest()
+opDebounceTest = opDebounce(debouncedPrinting, 5000, true)
+opDebounceTest()
