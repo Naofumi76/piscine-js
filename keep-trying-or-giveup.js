410635f80bc4657d5fdaae96017dca836b81143c
nsannier <Naofumi76@users.noreply.github.com>
2025-01-23 09:02:48 +0100

    keep-trying-or-giveup

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -24,23 +24,3 @@ function timeout(delay=0, callback = async () => {}) {
         return Promise.race([callbackPromise, timeoutPromise])
     }
 }
-
-async function failingCallback() {
-    console.log('Attempt...')
-    throw new Error('Failure')
-}
-
-async function successfulCallback() {
-    console.log('Processing...')
-    return new Promise((resolve) => setTimeout(() => resolve('Success'), 500))
-}
-
-const retryWith3Attempts = retry(3, failingCallback)
-retryWith3Attempts().catch(console.error);
-
-
-const timeoutWith1Sec = timeout(1000, successfulCallback)
-timeoutWith1Sec().then(console.log).catch(console.error)
-
-const timeoutWith500ms = timeout(500, successfulCallback)
-timeoutWith500ms().then(console.log).catch(console.error)
