eb96fca5208cf3976e7fd908b51e5a2f684c0be9
nsannier <Naofumi76@users.noreply.github.com>
2025-01-23 08:17:03 +0100

    interpolation

--- interpolation.js
+++ interpolation.js
@@ -4,30 +4,14 @@ function interpolation({ step = 0, start =0, end=0, callback=()=>{}, duration =0
     var index = 0
     var timeout = setInterval(()=> {
         if (index < step) {
-            clearInterval(timeout)
-            return
-        }
-        actual += deltaX
-        callback([actual.toFixed(2), index])
-        index++
-    }, duration/step)
-}
-
-/* function interpolation({ step = 0, start =0, end=0, callback=()=>{}, duration =0} = {}) {
-    var deltaX = (end - start) / step
-    var actual = start
-    var index = 0
-    var timeout = setInterval(()=> {
-        if (index < step) {
             callback([actual, (duration / step) * (index+1)])
-            current += deltaX
+            actual += deltaX
             index++
+        } else {
+            clearInterval(timeout)
         }
-        actual += deltaX
-        callback([actual.toFixed(2), index])
-        index++
-    }, duration/step)
-} */
+    })
+}
 
 
 interpolation({
@@ -36,6 +20,6 @@ interpolation({
     end: 1,
     duration: 10,
     callback: ([x, y]) => {
-        console.log(`Point: [${x.toFixed(2)}, ${y.toFixed(2)}]`);
+        console.log(`Point: [${x}, ${y}]`);
     }
 });
